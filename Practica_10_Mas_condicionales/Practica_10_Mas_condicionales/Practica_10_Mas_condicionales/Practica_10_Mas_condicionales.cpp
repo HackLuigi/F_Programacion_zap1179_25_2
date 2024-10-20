bcb47c8f6d115e8e937178a561a1981c2dc9632d
HackLuigi <rubenjrnarro@gmail.com>
2024-10-20 07:51:03 -0600

    Tarea 10

--- Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales.cpp
+++ Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales.cpp
@@ -7,11 +7,61 @@ using namespace std;
 
 int main()
 {
-    bool Robot = false;
-    bool identidad = false;
-    while (Robot == false)
-    { 
-
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    bool ingresar = false;
+    bool mascota = false;
+    int edad;
+    int edad_u = 70;
+    string sexo;
+    string sexo_u = "M";
+    string Contra_u = "XHA";
+    string Contra;
+    while (ingresar == false)
+    {
+        system("cls");
+        cout << "Ok, creo que ya tienes anotada la contraseña verdad? Si no aqui te la vuelvo a escribir: XHA. Lo demas es muy obvio, tienes 70, eres Mujer o solo M y tienes un perro, buena suerte" << endl;
+        cout << "Porfavor escriba la contraseña para ingresar al fornais" << endl;
+        cin >> Contra;
+        if (Contra == Contra_u)
+        {
+            cout << "Escriba su sexo porfavor" << endl;
+            cin >> sexo;
+            if (sexo == sexo_u)
+            {
+                cout << "Porfavor escriba su edad" << endl;
+                cin >> edad;
+                if (edad == edad_u)
+                {
+                    cout << "Y ya para finalizar, toca la pregunta de seguridad ¿Tiene un perro o un gato? (1 para gato y 0 para perro)" << endl;
+                        cin >> mascota;
+                        if (mascota == false)
+                        {
+                            cout << "Muy bien señora, disfrute de su partida de fornais";
+                            ingresar = true;
+                        }
+                        else
+                        {
+                            cout << "Incorrecto, cerrando para tomar medidas de seguridad";
+                            ingresar = true;
+                        }
+                }
+                else
+                {
+                    cout << "Incorrecto, cerrando para tomar medidas de seguridad";
+                    ingresar = true;
+                }
+            }
+            else
+            {
+                cout << "incorrecto, cerrando para tomar medidas de seguridad";
+                ingresar = true;
+            }
+        }
+        else
+        {
+            cout << "No se reconoce la contraseña, intentelo denuevo" << endl;
+            system("pause");
+        }
     }
 }
 
