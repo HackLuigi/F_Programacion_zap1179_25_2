0ed8516b1592680aff51e2d660dc3f1c5cccedae
HackLuigi <rubenjrnarro@gmail.com>
2024-10-20 08:46:54 -0600

    Tarea 13

--- Practica_13_Aun_mas_bucles/Practica_13_Aun_mas_bucles/Practica_13_Aun_mas_bucles/Practica_13_Aun_mas_bucles.cpp
+++ Practica_13_Aun_mas_bucles/Practica_13_Aun_mas_bucles/Practica_13_Aun_mas_bucles/Practica_13_Aun_mas_bucles.cpp
@@ -2,10 +2,66 @@
 //
 
 #include <iostream>
+#include <locale>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string usuario;
+    string usuario_2;
+    string usuario_p = "Mario_64";
+    string contra;
+    string contra_2;
+    string contra_u = "Yahoo_2";
+    bool comprobacion = false;
+    setlocale(LC_ALL, "es_MX.UTF-8");
+    while (comprobacion == false)
+    {
+        system("cls");
+        cout << "Usuario?" << endl;
+        cin >> usuario;
+        if (usuario == usuario_p)
+        {
+            cout << "Hazlo otra vez" << endl;
+            cin >> usuario_2;
+            if (usuario_2 == usuario_p)
+            {
+                cout << "Contraseña?" << endl;
+                cin >> contra;
+                if (contra == contra_u)
+                {
+                    cout << "Hazlo otra vez" << endl;
+                    cin >> contra_2;
+                    if (contra_2 == contra_u)
+                    {
+                        cout << "Muy bien, disfrute de su juego";
+                        comprobacion = true;
+                    }
+                    else
+                    {
+                        cout << "BEEEP! Incorrecto! Vuelve a intentarlo!" << endl;
+                        system("pause");
+                    }
+                }
+                else
+                {
+                    cout << "BEEP! INCORRECTO! VUELVE A INTENTARLO!" << endl;
+                    system("pause");
+                }
+            }
+            else
+            {
+                cout << "BEEEP! Incorrecto! Vuelve a intentarlo!" << endl;
+                system("pause");
+            }
+        }
+        else
+        {
+            cout << "BEEEP! Incorrecto! Vuelve a intentarlo!" << endl;
+            system("pause");
+        }
+    }
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
