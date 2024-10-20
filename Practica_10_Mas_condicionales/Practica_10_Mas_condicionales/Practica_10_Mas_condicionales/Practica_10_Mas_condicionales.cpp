ecaa56a363796c1f487bbc8bd0d57530d05ff5df
HackLuigi <rubenjrnarro@gmail.com>
2024-10-20 02:21:28 -0600

    Tarea 11

--- Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales.cpp
+++ Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales/Practica_10_Mas_condicionales.cpp
@@ -1,71 +1,22 @@
-// Practica_10_Mas_condicionales.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// Practica_10_Mas_condicionales.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí
 
 #include <iostream>
 #include <locale>
+#include <cstdlib>
+using namespace std;
 
 int main()
 {
-    int decision;
-    int adivinacion;
-    bool pregunta = false;
-    bool pregunta_2 = false;
-    bool pregunta_3 = false;
-    bool otra_vez = true;
-    setlocale(LC_ALL, "es_MX.UTF-8");
-    while (otra_vez == false)
-    std::cout << "Empecemos ¿Que te gustaria adivinar? (Personas del salon 1. Animales 2. Comida 3. Juegos 4.)\n";
-    std::cin >> decision;
-    switch (decision)
-    {
-    case 1:
-        std::cout << "¿Tu persona del salon tiene barba? (1 si 0 no)" << std::endl;
-            std::cin >> adivinacion;
-            if (pregunta == true)
-            {
-                std::cout << "¿Tu persona del salon tiene lentes?" << std::endl;
-                std::cin >> pregunta_2;
-                if (pregunta_2 == true)
-                {
-                    std::cout << "a";
-                }
-                else
-                {
-                    std::cout << "¿Tu persona del salon tiene un bigote junto con su barba?";
-                }
-            }
-            else
-            {
-                std::cout << "¿Tu persona del salon tiene lentes?" << std::endl;
-                std::cin >> pregunta_2;
-                if (pregunta_2 == true)
-                {
-                    std::cout << "¿Tu persona del salon tiene lentes?";
-                }
-                else
-                {
-                    std::cout << "No hay nadie en el salon sin barba y lentes aqui";
-                }
-            }
-        break;
+    bool Robot = false;
+    bool identidad = false;
+    while (Robot == false)
+    { 
 
-    case 2:
-
-        break;
-
-    case 3:
-
-        break;
-
-    case 4:
-
-        break;
     }
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
-
 // Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
 //   2. Use la ventana de Team Explorer para conectar con el control de código fuente
 //   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
