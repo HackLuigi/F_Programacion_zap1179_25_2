c44db69ce1b9d88ae81f21559ed6b980292f4020
HackLuigi <rubenjrnarro@gmail.com>
2024-11-07 21:57:46 -0600

    Tarea

---
+++ P-16/P-16/P-16.cpp
@@ -0,0 +1,43 @@
+// P-16.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string nombre[3] = { "Mario bros wii","Smash bros 64","Metal gear solid" };
+    int epoca[3] = {2009, 1999, 1998};
+    string Creador[3] = { "Shigeru Miyamoto", "Masahiro Sakurai", "Hideo Kojima" };
+    string estudio[3] = { "Nintendo", "Nintendo", "Fox Studios" };
+    for (int list1 = 0; list1 < 3; list1++) 
+    {
+        cout << nombre[list1] << " ";
+    }
+    cout << endl;
+    for (int list2 = 0; list2 < 3; list2++)
+    {
+        cout << epoca[list2] << " ";
+    }
+    cout << endl;
+    for (int list3 = 0; list3 < 3; list3++)
+    {
+        cout << Creador[list3] << " ";
+    }
+    cout << endl;
+    for (int list4 = 0; list4 < 3; list4++)
+    {
+        cout << estudio[list4] << " ";
+    }
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
