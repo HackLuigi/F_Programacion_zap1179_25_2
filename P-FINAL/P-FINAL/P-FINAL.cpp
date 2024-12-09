218b3895782cc2d701a7e60d0178ee61a492564f
HackLuigi <rubenjrnarro@gmail.com>
2024-12-09 02:42:35 -0600

    Final
    A ver que pasa

--- P-FINAL/P-FINAL/P-FINAL.cpp
+++ P-FINAL/P-FINAL/P-FINAL.cpp
@@ -12,7 +12,7 @@ int main()
     srand(time(NULL));
     int decision;
     int finales = 0 % 8;
-    int moneda = rand() % 2;
+    int moneda = rand() % 21;
     bool otra_vez = true;
     string player;
     string chara = "Hazuki";
@@ -21,10 +21,10 @@ int main()
     while (otra_vez == true)
     {
         system("cls");
-        cout << "La vida es peculiar, a veces puedes tomar caminos que no esperas nunca que te hubieran aparecido, o caminos que el tu del pasado jamas hubiera tomado";
+        cout << "La vida es peculiar, a veces puedes tomar caminos que no esperas nunca que te hubieran aparecido, o caminos que el tu del pasado jamas hubiera tomado" << endl;
         system("pause");
         system("cls");
-        cout << "Estabas pensando en esto y otras cosas mas mientras caminabas hasta que terminas chocando con un chica, cuando abres los ojos y la ves, te das cuenta que es bastante linda, tiene cabello y ojos rojos y una cara bastante amigable cuando te das cuenta que te esta observando de vuelta";
+        cout << "Estabas pensando en esto y otras cosas mas mientras caminabas hasta que terminas chocando con un chica, cuando abres los ojos y la ves, te das cuenta que es bastante linda, tiene cabello y ojos rojos y una cara bastante amigable cuando te das cuenta que te esta observando de vuelta" << endl;
         system("pause");
         system("cls");
         cout << "???: 'Ay lo siento ¿Estas bien?'" << endl;
@@ -42,21 +42,21 @@ int main()
             cout << "???: 'Mi nombre es " << chara << " ¿Como se llama usted?'" << endl;
             system("pause");
             system("cls");
-            cout << "'Mi nombre? Pues yo me llamo '";
+            cout << "'Mi nombre? Pues yo me llamo... '" << endl;
             cin.ignore();
             getline(cin, player);
             system("pause");
             system("cls");
-            cout << chara << ": 'Ugh, mi ropa ya esta un poco manchada, y parece que voy a llegar un poco tarde al trabajo, pero bueno, cosas que pasan ¿No?'";
+            cout << chara << ": 'Ugh, mi ropa ya esta un poco manchada, y parece que voy a llegar un poco tarde al trabajo, pero bueno, cosas que pasan ¿No?'" << endl;
             system("pause");
             system("cls");
-            cout << "'Si, cosas que pasan";
+            cout << "'Si, cosas que pasan" << endl;
             system("pause");
             system("cls");
-            cout << "Ambos de ustedes rien un poco hasta que te das cuenta que tiene un gafete con una foto de ella junto a su nombre, el diseño del gafete se te hace conocido";
+            cout << "Ambos de ustedes rien un poco hasta que te das cuenta que tiene un gafete con una foto de ella junto a su nombre, el diseño del gafete se te hace conocido" << endl;
             system("pause");
             system("cls");
-            cout << "'Espera ¿Donde trabajas?";
+            cout << "'Espera ¿Donde trabajas?" << endl;
             system("pause");
             system("cls");
             cout << chara << ": 'Ah, trabajo en Capcom(R)" << endl;
@@ -65,61 +65,65 @@ int main()
             switch (decision)
             {
             case 1:
-                cout << chara << ": '¿¡En serio!? ¡Que genial! ¿Como es que no nos hemos visto ni en la calle ni en las oficinas antes?'";
+                cout << chara << ": '¿¡En serio!? ¡Que genial! ¿Como es que no nos hemos visto ni en la calle ni en las oficinas antes?'" << endl;
                 system("pause");
                 system("cls");
-                cout << "'Justo cuando estaban por seguir hablando mas, tu reloj suena, señalando de que casi es hora de estar en la oficina'";
+                cout << "'Justo cuando estaban por seguir hablando mas, tu reloj suena, señalando de que casi es hora de estar en la oficina'" << endl;
                 system("pause");
                 system("cls");
                 cout << chara << ": '¡Ay no! Supongo que es hora de irnos ¿Porque no caminamos juntos mientras hablamos al trabajo?'" << endl;
-                cout << "3. Claro, me encantaria 4. No, gracias" << endl;
+                cout << "1. Claro, me encantaria 2. No, gracias" << endl;
                 cin >> decision;
                 switch (decision)
                 {
-                case 3:
+                case 1:
                     finales += 1;
                     cout << chara << ": 'SIII!'" << endl;
-                    cout << "Despues de eso, " << chara << " y tu pasan mas tiempo juntos, conociendose y haciendose mas cercanos hasta que un dia se enamoran";
+                    cout << "Despues de eso, " << chara << " y tu pasan mas tiempo juntos, conociendose y haciendose mas cercanos hasta que un dia se enamoran" << endl;
                     system("pause");
                     system("cls");
-                    cout << "Final 1 de 7: Final feliz";
+                    cout << "Final 1 de 7: Final feliz" << endl;
+                    system("pause");
+                    system("cls");
 
                     break;
 
-                case 4:
-                    cout << chara << ": 'Oh... Ok... Disculpame... Voy a tomar un atajo...'";
+                case 2:
+                    cout << chara << ": 'Oh... Ok... Disculpame... Voy a tomar un atajo...'" << endl;
                     system("pause");
                     system("cls");
-                    cout << chara << " abandona el lugar, dejandote solo hacia el edificio del trabajo, horas despues, cuando sales del trabajo, algo fuerte te pega y te noquea, despertando, te das cuenta que estas en lo que parece ser un pequeño apartamento y entonces aparece " << chara << ", con una macabra sonrisa en su cara";
+                    cout << chara << " abandona el lugar, dejandote solo hacia el edificio del trabajo, horas despues, cuando sales del trabajo, algo fuerte te pega y te noquea, despertando, te das cuenta que estas en lo que parece ser un pequeño apartamento y entonces aparece " << chara << ", con una macabra sonrisa en su cara" << endl;
                     system("pause");
                     system("cls");
-                    cout << chara << ": 'Jeje, pensaste que escaparias de mi ¿Verdad? Pero no, no importa cuanto intentes esconderte y huir, te encontrare y obtendre";
+                    cout << chara << ": 'Jeje, pensaste que escaparias de mi ¿Verdad? Pero no, no importa cuanto intentes esconderte y huir, te encontrare y obtendre" << endl;
                     system("pause");
                     system("cls");
-                    cout << chara << ": 'Hagamos algo, juguemos un juego, lanzare una moneda, si da cara, te podras ir, si da cruz, tendras que morir'";
+                    cout << chara << ": 'Hagamos algo, juguemos un juego, lanzare una moneda, si da cara, te podras ir, si da cruz, tendras que morir'" << endl;
                     system("pause");
                     system("cls");
-                    if (moneda == 1)
+                    if (moneda >= 10)
                     {
                         finales += 1;
-                        cout << "Sale cara";
+                        cout << "Sale cara" << endl;
                         system("pause");
                         system("cls");
-                        cout << chara << ": 'Diablos! Bueno, te puedes ir, pero solo por ahora, la proxima te tendre y seras mio";
+                        cout << chara << ": 'Diablos! Bueno, te puedes ir, pero solo por ahora, la proxima te tendre y seras mio" << endl;
                         system("pause");
                         system("cls");
-                        cout << "Final 5 de 7: ESCAPA!";
+                        cout << "Final 5 de 7: ESCAPA!" << endl;
                         system("pause");
                         system("cls");
                     }
-                    else if (moneda == 0)
+                    else if (moneda <= 10)
                     {
                         finales += 1;
-                        cout << "Sale cruz";
-                        cout << chara << ": Muy bien! Entonces, preparate para vivir por el resto de tu dolorosa vida conmigo, querido";
+                        cout << "Sale cruz" << endl;
                         system("pause");
                         system("cls");
-                        cout << "Final 6 de 7: Final malo";
+                        cout << chara << ": Muy bien! Entonces, preparate para vivir por el resto de tu dolorosa vida conmigo, querido" << endl;
+                        system("pause");
+                        system("cls");
+                        cout << "Final 6 de 7: Final malo" << endl;
                         system("pause");
                         system("cls");
                     }
@@ -137,10 +141,10 @@ int main()
                 {
                 case 1:
                     finales += 1;
-                    cout << "Despues de eso, has visto a " << chara << " y hablado seguido y se hicieron buenos amigos, pero solo termino en eso, una simple y sencilla amistad y nada romantico";
+                    cout << "Despues de eso, has visto a " << chara << " y hablado seguido y se hicieron buenos amigos, pero solo termino en eso, una simple y sencilla amistad y nada romantico" << endl;
                     system("pause");
                     system("cls");
-                    cout << "Final 4 de 7: Amistad";
+                    cout << "Final 4 de 7: Amistad" << endl;
                     system("pause");
                     system("cls");
 
@@ -151,10 +155,10 @@ int main()
                     finales += 1;
                     system("pause");
                     system("cls");
-                    cout << "Despues de eso, te pega y despues no la has vuelto a ver nunca en ti vida";
+                    cout << "Despues de eso, te pega y despues no la has vuelto a ver nunca en ti vida" << endl;
                     system("pause");
                     system("cls");
-                    cout << "Final 2 de 7: Grosero";
+                    cout << "Final 2 de 7: Grosero" << endl;
                     system("pause");
                     system("cls");
 
@@ -175,10 +179,10 @@ int main()
                 finales += 1;
                 system("pause");
                 system("cls");
-                cout << "Despues de eso, te pega y no la has vuelto a ver nunca en ti vida";
+                cout << "Despues de eso, te pega y no la has vuelto a ver nunca en ti vida" << endl;
                 system("pause");
                 system("cls");
-                cout << "Final 2 de 7: Grosero";
+                cout << "Final 2 de 7: Grosero" << endl;
                 system("pause");
                 system("cls");
 
@@ -187,17 +191,17 @@ int main()
             case 2:
 
                 finales += 1;
-                cout << "Justo cuando la chica te iba a pegar, se detiene";
+                cout << "Justo cuando la chica te iba a pegar, se detiene" << endl;
                 system("pause");
                 system("cls");
-                cout << "La chica respira hondo y suspira";
+                cout << "La chica respira hondo y suspira" << endl;
                 system("pause");
                 system("cls");
                 cout << "???: 'Yo tambien me disculpo, la verdad creo que tambien me porte un poco agresiva, pero supongo que es la culpa de nuestros trabajos y horarios ¿eh?";
-                cout << "Despues de eso, ambos quedaron en buenos terminos y siguieron sus caminos, y despues de eso, no la has vuelto a ver, asi que se podria decir que solo fue un encuentro curioso";
+                cout << "Despues de eso, ambos quedaron en buenos terminos y siguieron sus caminos, y despues de eso, no la has vuelto a ver, asi que se podria decir que solo fue un encuentro curioso" << endl;
                 system("pause");
                 system("cls");
-                cout << "Final 7 de 7: Un pequeño encuentro";
+                cout << "Final 7 de 7: Un pequeño encuentro" << endl;
                 system("pause");
                 system("cls");
 
@@ -214,14 +218,41 @@ int main()
             cout << "Solo te alejas de la chica y nunca mas la has visto despues de eso, han pasado años, y ahora eres viejo y te das cuenta que probablemente vas a morir solo, mientras que tu mente no deja de preguntarse ¿Que hubiera pasado si le hubieras dirigido la palabra a aquella chica?" << endl;
             system("pause");
             system("cls");
-            cout << "Final 3 de 7: Pesar";
+            cout << "Final 3 de 7: Pesar" << endl;
             system("pause");
             system("cls");
             break;
         }
-        cout << "Tienes " << finales << " de 7 finales ¿Te gustaria jugar otra vez? (1 si 0 no)" << endl;
-        cin >> otra_vez;
+        if (finales >= 7)
+        {
+            cout << "Encuentras una puerta entrecerrada" << endl;
+            cout << "1. Reiniciar 2. Cerrar juego 3. Revisar" << endl;
+            cin >> decision;
+            switch (decision)
+            {
+            case 1:
 
+                break;
+
+            case 2:
+
+                otra_vez = false;
+
+                break;
+
+            case 3:
+
+                cout << "Abres la puerta y obtienes la papa dorada, como premio y agradecimiento por completar el juego" << endl;
+                otra_vez = false;
+
+                break;
+            }
+        }
+        else
+        {
+            cout << "Tienes " << finales << " de 7 finales ¿Te gustaria jugar otra vez? (1 si 0 no)" << endl;
+            cin >> otra_vez;
+        }
     }
 }
 
